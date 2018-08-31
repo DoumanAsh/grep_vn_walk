f7592d25c6b9b4437c4310a225740adcc2008dc3
Douman <douman@gmx.se>
2018-08-31 17:43:51 +0300

    0.2.0: Update to yukikaze and ignore case

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 #[macro_use(set_panic_message)]
 extern crate lazy_panic;
-extern crate encoding;
 extern crate regex;
+extern crate yukikaze;
 
 mod cli;
-mod actix;
 
-use self::regex::Regex;
-use self::actix::{Future, HttpMessage};
-use self::encoding::types::{Encoding, DecoderTrap};
-use self::encoding::codec::japanese::Windows31JEncoding as ShiftJS;
+use regex::Regex;
+use yukikaze::futures::Future;
+use yukikaze::rt::{AutoClient, AutoRuntime};
+use yukikaze::client::Request;
+use yukikaze::encoding::types::{Encoding, DecoderTrap};
+use yukikaze::encoding::codec::japanese::Windows31JEncoding as ShiftJS;
 
 const WALK_ROOT: &'static str = "http://seiya-saiga.com/game/";
 
@@ -23,27 +24,27 @@ fn print_walks(html: String) {
 fn get_walk<F: FnOnce(String) -> ()>(printer: F) {
     const URL: &'static str = "http://seiya-saiga.com/game/kouryaku.html";
 
-    let mut system = actix::System::new("grep_vn_walk");
-
-    let req = actix::get(URL).finish().expect("To create empty HTTP request")
+    let _ = Request::get(URL).expect("To create request")
+                             .empty()
                              .send()
-                             .map_err(|error| eprintln!("Error: {}", error))
-                             .and_then(|rsp| rsp.body().limit(usize::max_value()).map_err(|error| eprintln!("Error reading body: {}", error)))
+                             .map_err(|error| eprintln!("Error: {:?}", error))
+                             .and_then(|rsp| rsp.body().limit(u64::max_value()).map_err(|error| eprintln!("Error reading body: {:?}", error)))
                              .and_then(|body| ShiftJS.decode(&body, DecoderTrap::Strict).map_err(|error| eprintln!("Failed to decode ShiftJS: {}", error)))
-                             .map(printer);
-
-    let _ = system.run_until_complete(req);
-
+                             .map(printer)
+                             .finish();
 }
 
 fn main() {
     set_panic_message!(lazy_panic::formatter::Simple);
 
+    let _guard = yukikaze::rt::init();
+    yukikaze::rt::set_default();
+
     match cli::args() {
         cli::Args::Full => get_walk(print_walks),
         cli::Args::Grep(title) => get_walk(move |html| {
             let regex = Regex::new("<td align=\"left\"><B><A href=\"(?P<url>[^\"]+)\">(?P<title>[^<]+)</A></B></td>").expect("To create regex");
-            for caps in regex.captures_iter(&html).filter(|caps| caps["title"].contains(&title)) {
+            for caps in regex.captures_iter(&html).filter(|caps| caps["title"].to_lowercase().contains(&title)) {
                 println!("{} - {}{}", &caps["title"], WALK_ROOT, &caps["url"]);
             }
         }),
